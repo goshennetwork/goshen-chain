97a2b37bc200c42ed18f6d4d945ed77c10fe3675
syw-zj <115963626+syw-zj@users.noreply.github.com>
2022-08-09 11:54:07 +0800

    compiled success

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 #![no_std]
+#![no_main]
 
+use core::str::FromStr;
 use ethereum_types::H256;
-use hex::FromHex;
+use rustc_hex::FromHex;
 
 use common_types::bytes::ToPretty;
 use ethcore::state::riscv_db::RiscvDB;
 use hash_db::HashDB;
 use riscv_l2chain::state_transition_to_header;
 
-fn main() {
+#[no_mangle]
+pub extern "C" fn _start() {
     let mut db = RiscvDB::new(&[]);
     // let contents = fs::read_to_string("./batch.data").unwrap();
     // for line in contents.lines() {
     //     db.insert(&line.from_hex().unwrap());
     // }
-    let buffer = <[u8; 32]>::from_hex("eb611f33cdb62869b3c051d5c00021cc7e407095af80b50645b7ddc33aa92541")?;
-    let hash = H256::from(buffer);
+    let hash = H256::from_str("eb611f33cdb62869b3c051d5c00021cc7e407095af80b50645b7ddc33aa92541").unwrap();
     let header = state_transition_to_header(db, hash);
-    println!("0x{}", header.hash().to_hex());
+    // println!("0x{}", header.hash().to_hex());
 }
