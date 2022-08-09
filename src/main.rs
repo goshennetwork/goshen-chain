147c213f9886cc4b180b3cf0753d118ee68a4cad
qiluge <19525062+qiluge@users.noreply.github.com>
2022-08-09 15:49:12 +0800

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+extern crate riscv_evm;
+
 use core::str::FromStr;
 use ethereum_types::H256;
 use rustc_hex::FromHex;
@@ -12,6 +14,7 @@ use riscv_l2chain::state_transition_to_header;
 
 #[no_mangle]
 pub extern "C" fn _start() {
+    riscv_evm::runtime::debug("start");
     let mut db = RiscvDB::new(&[]);
     // let contents = fs::read_to_string("./batch.data").unwrap();
     // for line in contents.lines() {
@@ -20,4 +23,6 @@ pub extern "C" fn _start() {
     let hash = H256::from_str("eb611f33cdb62869b3c051d5c00021cc7e407095af80b50645b7ddc33aa92541").unwrap();
     let header = state_transition_to_header(db, hash);
     // println!("0x{}", header.hash().to_hex());
+    // riscv_evm::runtime::debug(header.hash().to_hex().as_str());
+    riscv_evm::runtime::ret(header.hash().0);
 }
