f6fe5925149f178723abe7d98deca718327e11e7
qiluge <19525062+qiluge@users.noreply.github.com>
2022-08-09 15:57:01 +0800

    read entry from syscall

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ pub extern "C" fn _start() {
     // for line in contents.lines() {
     //     db.insert(&line.from_hex().unwrap());
     // }
-    let hash = H256::from_str("eb611f33cdb62869b3c051d5c00021cc7e407095af80b50645b7ddc33aa92541").unwrap();
+    let hash = H256::from(riscv_evm::runtime::input());
     let header = state_transition_to_header(db, hash);
     // println!("0x{}", header.hash().to_hex());
     // riscv_evm::runtime::debug(header.hash().to_hex().as_str());
