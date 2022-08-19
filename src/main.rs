5a1df55f534591649aa9a8652972ddcfb232e992
qiluge <19525062+qiluge@users.noreply.github.com>
2022-08-19 16:37:01 +0800

    remove cache from riscv db

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,13 @@ use rustc_hex::FromHex;
 use common_types::bytes::ToPretty;
 use riscv_l2chain::riscv_db::RiscvDB;
 use hash_db::HashDB;
-use riscv_l2chain::state_transition_to_header;
+use riscv_l2chain::state_transition;
 
 #[no_mangle]
 pub extern "C" fn _start() {
     riscv_evm::runtime::debug("start");
     let mut db = RiscvDB::new(&[]);
-    // let contents = fs::read_to_string("./batch.data").unwrap();
-    // for line in contents.lines() {
-    //     db.insert(&line.from_hex().unwrap());
-    // }
     let hash = H256::from(riscv_evm::runtime::input());
-    let header = state_transition_to_header(db, hash);
-    // println!("0x{}", header.hash().to_hex());
-    riscv_evm::runtime::ret(header.hash().0);
+    let header = state_transition(db, hash);
+    riscv_evm::runtime::ret(header.0);
 }
