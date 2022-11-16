7bb8083abadc5307f9560878d2f6dc119b92882b
syw-zj <115963626+syw-zj@users.noreply.github.com>
2022-11-16 16:34:02 +0800

    Sequencer: check output is same with block hash (#4)
    * update sequencer checker
    
    * add data flag for sequencer checker
    
    * add some comment

--- bin/std/src/main.rs
+++ bin/std/src/main.rs
@@ -4,20 +4,40 @@ use std::str::FromStr;
 use ethereum_types::H256;
 use rustc_hex::FromHex;
 
+use clap::Parser;
 use common_types::bytes::ToPretty;
 use ethcore::state::backend::ProofCheck;
 use goshen_chain::state_transition;
 use hash_db::HashDB;
 
+#[derive(Parser)]
+#[command(name = "Goshen Chain Sequencer Checker")]
+#[command(author = "Goshen Team")]
+#[command(version = "1.0")]
+struct Cli {
+    /// entry hash
+    #[arg(long, short)]
+    input: String,
+    /// batch state(hash of end block that batch included)
+    #[arg(long, short)]
+    output: String,
+    /// file path of batch data
+    #[arg(long, short)]
+    data: String,
+}
+
 fn main() {
-    let contents = fs::read_to_string("./batch.data").unwrap();
+    let cli: Cli = Cli::parse();
+    let contents = fs::read_to_string(cli.data).unwrap();
     let mut db = ProofCheck::new(&[]);
     for line in contents.lines() {
         let data: Vec<u8> = line.from_hex().unwrap();
         db.insert(&data);
     }
-    let hash =
-        H256::from_str("2a6b1be06ab62c350d6f42c16653280f6ffbda39ef3cc7fb64b478c329e45742").unwrap();
+    let input = if cli.input.starts_with("0x") { &cli.input[2..] } else { &cli.input };
+    let output = if cli.output.starts_with("0x") { &cli.output[2..] } else { &cli.output };
+
+    let hash = H256::from_str(input).unwrap();
     let header = state_transition(db, hash);
-    println!("0x{}", header.to_hex());
+    assert_eq!(output, header.to_hex());
 }
--- build.sh
+++ build.sh
@@ -2,3 +2,8 @@ cd bin/riscv
 cargo build --release --no-default-features -Z build-std=core,alloc --target ../../riscv32ima-unknown-none-elf.json --bin riscv-l2chain
 cd ../../
 cp target/riscv32ima-unknown-none-elf/release/riscv-l2chain .
+
+cd bin/std
+cargo build --release --bin sequencer-checker
+cd ../../
+cp target/release/sequencer-checker .
